ce2c9f164ef5bc9ef977061f306c4fb8e26134f8
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-08-02 19:20:57 +0300

    Add Entity(Company).

---
+++ src/main/java/com/luv2code/demo/entity/Company.java
@@ -0,0 +1,45 @@
+package com.luv2code.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "companies")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Company {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false, unique = true)
+	@NotBlank
+	private String name;
+
+	@Column(name = "createdAt")
+	private LocalDateTime createdAt;
+
+	@Column(name = "imageUrl", length = 1000, nullable = false)
+	private String imageUrl;
+
+	@PrePersist
+	protected void onCreate() {
+		createdAt = LocalDateTime.now();
+	}
+	
+}
